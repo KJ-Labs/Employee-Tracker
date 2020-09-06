c48e5958b425eb9547834aa5a51ecec848fc0e50
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-09-06 12:48:36 -0700

    Added delete employee function

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# Employee-Tracker
+
+
+![Git](background.PNG)  
+# Employee Tracker
+
+![Badge1](https://img.shields.io/badge/License-MIT-Blue)
+
+
+## Description 
+The purpose of this project is to use the command line to add/view departments, roles and employees. 
+
+## Project Demonstration
+![Git](readmevideo.gif)  
+
+
+## Table of contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [Repository Link](#repository)
+
+
+
+## Installation
+
+        Step 1.) Copy the Seed file into SQL, so that the tables are setup with some base data. 
+
+## Usage
+
+        Step 1.) Open up index.js in the command terminal, run node index.js 2.) Answer the questions depending on your use case. 
+
+## License
+MIT 
+
+
+## Contributing
+
+The owner of this project is KJ-Labs, you're welcome to contribute.
+
+
+## Questions
+
+- If you have any questions, please contact me on Github.
+- undefined
+- [KJ-Labs](https://github.com/KJ-Labs)
+
+## Repository
+
+- [Project Repo](https://github.com/KJ-Labs/Employee-Tracker)
+
+## Screenshots
+![Git](screenshot.PNG)  
+
+
--- index.js
+++ index.js
@@ -1,66 +1,72 @@
 const mysql = require('mysql');
 const inquirer = require("inquirer");
 
+//SQL Connection Info
+
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "password",
     database: "employee_db"
 });
 
+
+//SQL Connection Info
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     askQuestions();
 });
 
+//Begins the question prompt for user to decide what to do
 function askQuestions() {
     inquirer.prompt({
         message: "what would you like to do?",
         type: "list",
         choices: [
-            "view all employees",
-            "view all departments",
-            "add employee",
-            "add department",
-            "add role",
-            "update employee role",
-            "QUIT"
+            "1.) view all employees",
+            "2.) view all departments",
+            "3.) add employee",
+            "4.) add department",
+            "5.) add role",
+            "6.) update employee role",
+            "7.) delete an employee",
+            "8.) QUIT"
         ],
         name: "choice"
     }).then(answers => {
         console.log(answers.choice);
         switch (answers.choice) {
-            case "view all employees":
+            case "1.) view all employees":
                 viewEmployees()
                 break;
 
-            case "view all departments":
+            case "2.) view all departments":
                 viewDepartments()
                 break;
 
-            case "add employee":
+            case "3.) add employee":
                 addEmployee()
                 break;
 
-            case "add department":
+            case "4.) add department":
                 addDepartment()
                 break;
 
-            case "add role":
+            case "5.) add role":
                 addRole()
                 break;
 
-            case "update employee role":
+
+            case "6.) update employee role":
                 updateEmployeeRole();
                 break;
+
+            case "7.) delete an employee":
+                deleteEmployee();
+                break;
+
 
             default:
                 connection.end()
@@ -69,6 +75,7 @@ function askQuestions() {
     })
 }
 
+//Queries all employees
 function viewEmployees() {
     connection.query("SELECT * FROM employee", function (err, data) {
         console.table(data);
@@ -76,6 +83,7 @@ function viewEmployees() {
     })
 }
 
+//Queries all departments
 function viewDepartments() {
     connection.query("SELECT * FROM department", function (err, data) {
         console.table(data);
@@ -83,6 +91,7 @@ function viewDepartments() {
     })
 }
 
+//Add a new employee
 function addEmployee() {
     inquirer.prompt([{
             type: "input",
@@ -113,6 +122,7 @@ function addEmployee() {
     })
 }
 
+//Add a new department
 function addDepartment() {
     inquirer.prompt([{
         type: "input",
@@ -127,6 +137,8 @@ function addDepartment() {
     })
 }
 
+
+//Add a role
 function addRole() {
     inquirer.prompt([
         {
@@ -145,28 +157,64 @@ function addRole() {
     ]).then(function (response) {
         connection.query("INSERT INTO roles (title, salary, department_id) values (?, ?, ?)", [response.title, response.salary, response.department_id], function (err, data) {
             console.table(data);
+     
         })
         askQuestions();
     })
 
 }
 
+//Update an Existing Role
 function updateEmployeeRole() {
     inquirer.prompt([
         {
-            message: "which employee would you like to update? (use first name only for now)",
+            message: "What is the first name of the employee would you like to update?",
             type: "input",
             name: "name"
-        }, {
+        }, 
+        {
+            message: "What is the last name of the employee would you like to update?",
+            type: "input",
+            name: "last_name"
+        },   
+        {
             message: "enter the new role ID:",
             type: "number",
             name: "role_id"
         }
     ]).then(function (response) {
-        connection.query("UPDATE employee SET role_id = ? WHERE first_name = ?", [response.role_id, response.name], function (err, data) {
+        connection.query("UPDATE employee SET role_id = ? WHERE first_name = ? and last_name = ?", [response.role_id, response.name, response.last_name], function (err, data) {
             console.table(data);
+      
         })
         askQuestions();
     })
+
+}
+
+//Delete an employee
+function deleteEmployee() {
+  inquirer.prompt([
+      {
+          message: "What is the first name of the employee you would like to delete? ",
+          type: "input",
+          name: "name"
+      }, {
+          message: "What is the employee's last name?",
+          type: "input",
+          name: "last_name"
+      },  
+        {
+            message: "What is their employee ID:",
+            type: "number",
+            name: "empl_id"
+        }
+  ]).then(function (response) {
+      connection.query("Delete from employee WHERE first_name = ? AND last_name = ? AND employee_id = ? ", [response.name, response.last_name], response.empl_id, function (err, data) {
+          console.table(data);
+         
+      })
+      askQuestions();
+  })
 
 }
